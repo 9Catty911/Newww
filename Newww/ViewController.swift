05fa0d5658ac010c4d59d4fd09da9ca11c2c1b6d
Programming <programming@macbook-pro-programming-5.local>
2019-01-12 15:48:49 +0300

    Initial Commit

---
+++ Newww/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newww
+//
+//  Created by Programming on 12.01.2019.
+//  Copyright © 2019 MAPF games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
